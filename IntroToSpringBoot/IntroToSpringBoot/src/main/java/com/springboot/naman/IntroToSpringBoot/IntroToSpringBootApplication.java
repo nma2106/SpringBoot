4944fa3fae4336112c899b327718fb9959125d79
nma2106 <aryannaman51@gmail.com>
2024-07-23 21:30:00 +0530

    added intro files

---
+++ IntroToSpringBoot/IntroToSpringBoot/src/main/java/com/springboot/naman/IntroToSpringBoot/IntroToSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.naman.IntroToSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroToSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroToSpringBootApplication.class, args);
+	}
+
+}
